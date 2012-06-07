a74d49ad6ff4b684d6450f04d2938b037c64b752
Jessé Alves Galdino <galdino.jesse@gmail.com>
2012-06-07 12:47:06 -0300

    * FosUserBundle configured

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,10 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+
+	    //non sf standard
+	    new FOS\UserBundle\FOSUserBundle(),
+            new Kpb\UserBundle\KpbUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Kpb/UserBundle/KpbUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Kpb\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class KpbUserBundle extends Bundle
+{
+}
