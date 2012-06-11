a96bbb93760340c8d2aa10f5aac7a3010be467b3
Jessé Alves Galdino <galdino.jesse@gmail.com>
2012-06-11 17:49:26 -0300

    .

--- src/Kpb/QuoteBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Kpb/QuoteBundle/Tests/Controller/DefaultControllerTest.php
@@ -3,15 +3,23 @@
 namespace Kpb\QuoteBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Kpb\UserBundle\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\AnonymousToken;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testCreate()
     {
-        $client = static::createClient();
+        //Test an unloged user in a protected area
+        $client = static::createClient(array(), array('HTTP_HOST' => 'doquote.me'));
+        $client->request('GET', '/create');
+        $this->assertTrue($client->getResponse()->isRedirect('http://doquote.me/login'));
+        
+        //Test an logged in user
+        $crawler = $client->request('GET', '/create', array(), array(), array('PHP_AUTH_USER' => 'test', 'PHP_AUTH_PW' => 'test'));
+        
+        var_dump($client->getResponse()->getContent()); die;
 
-        $crawler = $client->request('GET', '/hello/Fabien');
-
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $this->assertGreaterThan(0, $crawler->filter('form')->count());
     }
 }
